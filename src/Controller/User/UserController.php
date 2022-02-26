4afaa672cda9030f0eb591cc7607c13da825c318
antoinec <antoine.calmettes@coorty.fr>
2022-02-26 16:20:34 +0100

    get user

---
+++ src/Controller/User/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\User;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UserController extends AbstractController
+{
+ /**
+     * @Route("/users", name="get_user", methods={"GET"})
+     */
+    public function getUsers(Request $request,UserRepository $userRepo,SerializerInterface $serializer) :JsonResponse
+    {
+        $json = $serializer->serialize(['body'=>$userRepo->findAll(),'code'=>Response::HTTP_OK],'json',['groups'=>'user:read']);
+        return new JsonResponse($json,Response::HTTP_OK,[],true);
+    }
+}
