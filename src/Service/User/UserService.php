b1453f4477645814b4b9a27ea17d636d8b96e6ef
antoinec <antoine.calmettes@coorty.fr>
2022-04-02 15:05:16 +0200

    api errors

--- src/Service/User/UserService.php
+++ src/Service/User/UserService.php
@@ -16,12 +16,28 @@ class UserService
             if (in_array(User::ROLE_ADMIN, $user->getRoles())) {
                 return null;
             }
+            if(empty($user->getCompany())){
+                if($user->getId() !== $userRequest->getId()){
+                    return new JsonResponse([
+                        'Exception'=>"contact the administrator, your rights are not sufficient",
+                        "code"=>Response::HTTP_BAD_REQUEST
+                    ]);
+                }else{
+                    return null;
+                }
+            }
+
+            if(empty($userRequest->getCompany())){
+                return null;
+            }
             if ($user->getCompany()->getId() !== $userRequest->getCompany()->getId()) {
+                
                 return new JsonResponse([
                     'Exception'=>"contact the administrator, your rights are not sufficient",
                     "code"=>Response::HTTP_BAD_REQUEST
                 ]);
             }
+            return null;
         } catch (\Exception $e) {
             return new JsonResponse([
                 'Exception'=>$e->getMessage(),
